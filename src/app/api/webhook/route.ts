4381ce97a539ed763527e9f008ff017b515cb79c
Ashutosh <ashutoshnautiyal94@gmail.com>
2024-05-25 11:09:43 +0530

    added patch method to webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -38,63 +38,64 @@ const webhookHandler = async (req: NextRequest) => {
 		// Successfully constructed event.
 		console.log("✅ Success:", event.id);
 
-		// getting to the data we want from the event
-		const subscription = event.data.object as Stripe.Subscription;
-		const subscriptionId = subscription.id;
-
-    let news; 
 
 
-    if (event.type === 'customer.subscription.created') {
-      const subscription = event.data.object as Stripe.Subscription;
-    
-      // Get the customer ID from the subscription
-      const customerId = subscription.customer as string;
-    
-      // Retrieve the customer object from Stripe
-      const customer = await stripe.customers.retrieve(customerId);
-    
-      // Extract the billing details from the customer object
-      const billingDetails = customer;
+		switch (event.type) {
+			case "customer.subscription.created":
+				
+			// getting to the data we want from the event
+				const subscription = event.data.object as Stripe.Subscription;
+				const subscriptionId = subscription.id;
+				const customerId = subscription.customer as string;
 
-      news = billingDetails; 
-    
-      // Do something with the billing details
-      console.log('Billing details:', billingDetails);
-    }
+				// Retrieve the customer object from Stripe
+				const customer = await stripe.customers.retrieve(customerId);
+				
+				let customerEmail;
+
+					// Type guard to check if customer is of type Customer
+					if ('email' in customer && customer.email) {
+					customerEmail = customer.email;
+					} else {
+					console.error('Customer not found or has been deleted');
+					}
+
+				// Make a request to the Express server's /api/update-profile endpoint
+				const updateProfileResponse = await fetch(`https://we-out-backend.vercel.app/api/gen-update`, {
+					method: 'PATCH',
+					headers: {
+					'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+					email: customerEmail,
+					stripeCustomerId: customerId,
+					isActive: true,
+					subscriptionID: subscription.id,
+					}),
+				});
+			
+				if (updateProfileResponse.ok) {
+					console.log('User data updated successfully');
+				} else {
+					console.error('Failed to update user data');
+				}
+				
 
 
+				break;
+				case "customer.subscription.deleted":
+				
+				break;
 
-		// switch (event.type) {
-		// 	case "customer.subscription.created":
-		// 		await prisma.user.update({
-		// 			where: {
-		// 				stripeCustomerId: subscription.customer as string,
-		// 			},
-		// 			data: {
-		// 				isActive: true,
-		// 				subscriptionID: subscriptionId,
-		// 			},
-		// 		});
-		// 		break;
-		// 	case "customer.subscription.deleted":
-		// 		await prisma.user.update({
-		// 			where: {
-		// 				stripeCustomerId: subscription.customer as string,
-		// 			},
-		// 			data: {
-		// 				isActive: false,
-		// 			},
-		// 		});
-		// 		break;
+			default:
+				console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
+				break;
+		}
 
-		// 	default:
-		// 		console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
-		// 		break;
-		// }
 
-		// Return a response to acknowledge receipt of the event.
-		return NextResponse.json({news,  received: true });
+   
+
+		return NextResponse.json({  received: true });
 	} catch {
 		return NextResponse.json(
 			{
@@ -108,3 +109,69 @@ const webhookHandler = async (req: NextRequest) => {
 };
 
 export { webhookHandler as POST };
+
+
+
+// if (event.type === 'customer.subscription.created') {
+// 	const subscription = event.data.object as Stripe.Subscription;
+  
+// 	// Get the customer ID from the subscription
+// 	const customerId = subscription.customer as string;
+  
+// 	// Retrieve the customer object from Stripe
+// 	const customer = await stripe.customers.retrieve(customerId);
+  
+// 	// Extract the billing details from the customer object
+// 	const billingDetails = customer;
+
+// 	news = billingDetails; 
+  
+// 	// Do something with the billing details
+// 	console.log('Billing details:', billingDetails);
+//   }
+
+
+
+
+ // {
+    //   "news": {
+    //     "id": "cus_QADALsEOKYci3Q",
+    //     "object": "customer",
+    //     "address": {
+    //       "city": "146749",
+    //       "country": "IS",
+    //       "line1": "VILLAGE MISHAR GAON P.O BHATWARI DHANARI",
+    //       "line2": null,
+    //       "postal_code": "249193",
+    //       "state": null
+    //     },
+    //     "balance": 0,
+    //     "created": 1716536459,
+    //     "currency": "usd",
+    //     "default_source": null,
+    //     "delinquent": true,
+    //     "description": null,
+    //     "discount": null,
+    //     "email": "infernal@gmail.com",
+    //     "invoice_prefix": "E05446F8",
+    //     "invoice_settings": {
+    //       "custom_fields": null,
+    //       "default_payment_method": null,
+    //       "footer": null,
+    //       "rendering_options": null
+    //     },
+    //     "livemode": false,
+    //     "metadata": {
+    //     },
+    //     "name": "Ashu",
+    //     "next_invoice_sequence": 2,
+    //     "phone": "+917455841613",
+    //     "preferred_locales": [
+    //       "en-GB"
+    //     ],
+    //     "shipping": null,
+    //     "tax_exempt": "none",
+    //     "test_clock": null
+    //   },
+    //   "received": true
+    // }
